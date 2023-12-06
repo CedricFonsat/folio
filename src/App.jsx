c8bd1d340b4ba40ea5a9260107843ba9aa3b4242
Cedric Fonsat <cedricfonsat@MacBook-Air-de-Cedric.local>
2023-12-06 22:29:19 +0100

    privacy

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/home/Home'
+import Privacy from './pages/privacy/Privacy'
 import { Analytics } from '@vercel/analytics/react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-       <Home/>
-       <Analytics/>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} /> {/* 👈 Renders at /app/ */}
+      <Route path="/privacy" element={<Privacy />} /> {/* 👈 Renders at /app/ */}
+    </Routes>
+  </BrowserRouter>
   )
 }
 
